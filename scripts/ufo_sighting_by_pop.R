275009b2bba0ede5c3d8b94fa8617e410dd64358
Benjamin Bodek <benjamin.bodek@gmail.com>
2021-11-05 17:45:33 +0000

    Adding code to clean state fields and match ufo sightings to population from census data

--- scripts/ufo_sighting_by_pop.R
+++ scripts/ufo_sighting_by_pop.R
@@ -11,5 +11,19 @@ tidy_state <- state_df%>%
     names_to = "year", 
     values_to = "population", 
     names_prefix = "year_")%>%
-  mutate(year=substring(year, 2))
+  mutate(year=substring(year, 2))%>%
+  mutate(year=as.integer(year))%>%
+  mutate(state=substring(region,2))%>%
+  mutate(state=state.abb[match(state,state.name)])
+  
+sighting_df<-df%>%
+  group_by(state,year)%>%
+  tally()%>%
+  filter(is.na(state) == FALSE)%>%
+  filter(state!="")%>%
+  rename(sightings=n)%>%
+  mutate(year=as.integer(year))
 
+sighting_pop_df<-inner_join(sighting_df,tidy_state,by = c("state" = "state", "year"="year"))
+
+                           
