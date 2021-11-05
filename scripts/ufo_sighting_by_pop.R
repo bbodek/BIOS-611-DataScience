6dc991572d485318413d4ab03918406c1eed46cd
Benjamin Bodek <benjamin.bodek@gmail.com>
2021-11-05 17:57:51 +0000

    Adding code to convert population to an integer and create calculated field sightings_per_100k

--- scripts/ufo_sighting_by_pop.R
+++ scripts/ufo_sighting_by_pop.R
@@ -5,6 +5,7 @@ source("scripts/utils.R")
 df<-read.csv("./derived_data/nuforc_ufo_clean_data.csv")
 state_df<-read.csv("./source_data/census_est_pop.csv")
 
+# create tidy dataframe of state and population data from census population estimates
 tidy_state <- state_df%>%
   pivot_longer(
     cols = starts_with("X"), 
@@ -14,8 +15,11 @@ tidy_state <- state_df%>%
   mutate(year=substring(year, 2))%>%
   mutate(year=as.integer(year))%>%
   mutate(state=substring(region,2))%>%
-  mutate(state=state.abb[match(state,state.name)])
-  
+  mutate(state=state.abb[match(state,state.name)])%>%
+  mutate(population=strip_strings(population))%>%
+  mutate(population=as.integer(population))
+
+# create dataframe of the number of ufo sightings by state and year
 sighting_df<-df%>%
   group_by(state,year)%>%
   tally()%>%
@@ -24,6 +28,6 @@ sighting_df<-df%>%
   rename(sightings=n)%>%
   mutate(year=as.integer(year))
 
-sighting_pop_df<-inner_join(sighting_df,tidy_state,by = c("state" = "state", "year"="year"))
-
+sighting_pop_df<-inner_join(sighting_df,tidy_state,by = c("state" = "state", "year"="year"))%>%
+  mutate(sightings_per_100k = 100000*sightings/population)
                            
