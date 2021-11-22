a8182bf9a8d7acb688727c4a54f94e390680ddbd
Ben Bodek <benbodek@Bens-MacBook-Pro.local>
2021-11-21 22:27:15 -0500

    Fixing write to csv

--- scripts/tfidf_by_state.R
+++ scripts/tfidf_by_state.R
@@ -1,7 +1,6 @@
 library(tm)
 library(dplyr)
 library(cluster)
-library(NbClust)
 library(factoextra)
 
 source("scripts/utils.R")
@@ -22,30 +21,8 @@ states<-text_df%>%pull(state)
 
 tfidf.matrix<-tfidf(text_df$text)
 rownames(tfidf.matrix)<-states
-dist.matrix = proxy::dist(tfidf.matrix, method = "cosine")
-
-# see which method has the highest agglomerative coefficient (ac)
-m<- c( "average", "single", "complete", "ward")
-names(m) <- c( "average", "single", "complete", "ward")
-
-# function to compute coefficient
-ac <- function(x) {
-  agnes(dist.matrix, method = x)$ac
-}
-# create table of clustering methods and their AC score
-cluster_ac<-map_dbl(m, ac)
-best_method<-m[which.max(cluster_ac)]
-
-#nb<-NbClust(diss=dist.matrix,method="ward.D",,distance=NULL, min.nc=3,max.nc=20,index="dunn")
-
-clustering.hierarchical <- hclust(dist.matrix, method = best_method)
-h.clusters <- cutree(clustering.hierarchical, k = 5)
+df<-as.data.frame(tfidf.matrix)
+df <- tibble::rownames_to_column(df, "State")
 
 
-text_df$clusters<-unname(h.clusters)
-text_df%>% group_by(clusters)%>%tally()
-plot(clustering.hierarchical, cex = 0.6, hang = -1)
-rect.hclust(clustering.hierarchical, k = 5, border = 2:8)
-
-fviz_cluster(list(data = dist.matrix, cluster = h.clusters))
-write_csv(text_df,"derived_data/clustered_ufo_descriptions.csv")
+write_csv(df,"derived_data/ufo_description_tfidf.csv")
