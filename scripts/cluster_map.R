701e85650ba19f52ea1a8c9faeb55a84d47655e6
Ben Bodek <benbodek@Bens-MacBook-Pro.local>
2021-11-28 12:44:17 -0500

    Adding script to create map of clusters

--- scripts/cluster_map.R
+++ scripts/cluster_map.R
@@ -8,7 +8,13 @@ states<-data.frame("state_name"=tolower(state.name),"state_abb" = state.abb)
 
 df<-df%>%inner_join(states, by=c("state"="state_abb"))%>%inner_join(MainStates, by=c("state_name"="region"))
 
+png(file="./figures/cluster_map.png",width=7,height=4,units="in",res=350)
 ggplot() + 
   geom_polygon( data=df, aes(x=long, y=lat, group=group,fill=as.character(cluster)),
-                color="black") + labs(fill = "Cluster") 
+                color="black") + labs(fill = "Cluster",x="",y="") +
+  theme(axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks = element_blank(),
+        rect = element_blank())
 
+dev.off()
